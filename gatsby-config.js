b1b19f8de3c6df6ab27a238b1a034546c26cd387
Ammar Jabakji <ammarjabakji@gmail.com>
2019-08-30 16:26:18 +0300

    install gatsby-plugin-htaccess

--- gatsby-config.js
+++ gatsby-config.js
@@ -20,6 +20,7 @@ module.exports = {
   plugins: [
     "gatsby-plugin-react-helmet",
     "gatsby-plugin-sass",
+    "gatsby-plugin-htaccess",
     {
       resolve: "gatsby-source-filesystem",
       options: {
