057a8f8493fae25899da84faeedd35559cfc4d7f
Ammar Jabakji <ammarjabakji@gmail.com>
2019-08-30 15:53:36 +0300

    removing lodash

--- gatsby-config.js
+++ gatsby-config.js
@@ -20,7 +20,6 @@ module.exports = {
   plugins: [
     "gatsby-plugin-react-helmet",
     "gatsby-plugin-sass",
-    "gatsby-plugin-lodash",
     {
       resolve: "gatsby-source-filesystem",
       options: {
